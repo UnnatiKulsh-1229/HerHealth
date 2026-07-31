b9ee650f236c18853b1d0cc3e5b7421a60566704
Unnati Kulshrestha <unkl.dolly@gmail.com>
2026-08-01 01:03:23 +0530

    Fixed Aiven SSL

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,7 +7,7 @@ const db = mysql.createConnection({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   ssl: {
-    rejectUnauthorized: true,
+    rejectUnauthorized: false,
   },
 });
 
